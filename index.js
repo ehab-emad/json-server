4b9e132ead830553d9cb48578e83555d6c84e51d
ehab-emad <ehabemad515@gmail.com>
2024-07-16 01:55:35 +0200

    add1

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
+
+
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 30001;
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
+server.listen(port)
